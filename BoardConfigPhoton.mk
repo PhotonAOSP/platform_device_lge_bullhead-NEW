1fa8ae0bae8367364c0a687758014b645714b101
Shawn W <wahi.shawn@gmail.com>
2015-12-05 14:13:18 -0500

    Add toolchain parameters for inline kernel building

--- BoardConfigPhoton.mk
+++ BoardConfigPhoton.mk
@@ -1,7 +1,9 @@
 # Inline kernel
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-TARGET_KERNEL_SOURCE := kernel/lge/bullhead
 TARGET_KERNEL_CONFIG := bullhead_defconfig
+TARGET_KERNEL_SOURCE := kernel/lge/bullhead
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 # Enable vendor image symlink
 BOARD_NEEDS_VENDORIMAGE_SYMLINK := true
